9d8286625dde1cf3921c67b8c77cc3675e7300d4
Jordyn Hartzell <hartzellj@student.swosu.edu>
2015-11-11 17:42:00 -0600

    Cleaned up the code

--- src/Addition_Game_Methods_and_Loops.java
+++ src/Addition_Game_Methods_and_Loops.java
@@ -4,43 +4,49 @@ public class Addition_Game_Methods_and_Loops {
 		/* This line was used to make sure the program was running.
 		System.out.println("Hello");*/
 		
+		// Output opening message and instructions
+		System.out.println("Welcome to the addition game!");
+		System.out.println("Please input integers only.");
+		System.out.println("Good luck!\n");
+		
 		// Call the addition game method to run
 		additonGameMethod();
-	}
+	}	
 	// Create method for the addition game
 	public static void additonGameMethod(){
 		/* This was used to check that the method was working.
 		System.out.println("If this runs, additionGameMethod works");*/
 		
 		// Establish variables for difficulty and score
-		int score            = 0;
-		int difficulty       = 10;
-		int changeInDifficulty = 10;
+		int difficulty = 10;
+		int score = 0;
 		
 		
 		// For loop to increment 4 rounds
 		for(int round = 1; round <= 4; round++){
 			
-			// This boolean and corresponding method was copied from Jeremy Evert's Addition Game on Github.
+			// This boolean was copied from Jeremy Evert's Addition Game on Github.
 			boolean isAnswerCorrect = checkStudentAnswer(difficulty);
 			
-			// Outputs based on correct answer
+			// Output based on correct answer
 			if(isAnswerCorrect){
-				// Increase and display score
+				// Increase and output score
 				score = score + difficulty;
 				System.out.println("You have earned " + score + " points.");
 				
-				// Increase and display difficulty
-				if(round <= 4){
-					difficulty = difficulty * changeInDifficulty;
+				if(round < 4){
+					// Increase and output difficulty
+					difficulty = difficulty * 10;
 					System.out.println("Your difficulty has increased to " + difficulty + ".");
-			// Outputs based on incorrect answers
+				}
+			// Output based on incorrect answer
 			}else{
-				System.out.println("You still have " + score + " points.");
-				if(round <= 4 && difficulty > 10){
-					// Decrease difficulty
-					difficulty = difficulty / changeInDifficulty;
-					System.out.println("Your difficulty has decreased to " + difficulty + ".");
+				System.out.println("Your score is still " + score + ".");
+				if(round < 4){
+					if(difficulty > 10){
+						// Decrease and output difficulty
+						difficulty = difficulty / 10;
+						System.out.println("Your difficulty has decreased to " + difficulty + ".");
 					}
 				}
 				
